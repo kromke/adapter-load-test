c3731832ef214d11b8bf332eee87a13b1be9691b
rtarasenko <rtarasenko@phoenixit.ru>
2023-06-09 14:03:51 +0300

    Улучшил консьмер и отправщик

--- src/adapter_load_test/core.clj
+++ src/adapter_load_test/core.clj
@@ -6,81 +6,89 @@
   (:import [java.time Instant Duration])
   (:gen-class))
 
-(defn get-config [file] (read-string (slurp file)))
+(defn- get-config [file] (read-string (slurp file)))
 
-(defn send-messages [conf load]
-  (let [{:keys [kafka-servers topic headers]} conf
-        value (json/write-str (:value conf))
-        {:keys [requestType requestId traceKey] :or {requestType ""}} headers
-        requestId* (atom [])
-        ]
+(defn- get-fn-requestId? [record]
+  (fn [value] (contains?
+               (set
+                (map
+                 #(some->> (::K/headers %)
+                           (filter (fn [e] (= "requestId" (first e))))
+                           (apply second)
+                           String.)
+                 record))
+               value)))
+
+(defn- send-messages [{:keys [kafka-servers topic headers] :as conf} requestIds]
+  (let [value (json/write-str (:value conf))
+        {:keys [requestType traceKey] :or {requestType ""}} headers]
 
     (with-open [producer (K.out/producer {::K/nodes kafka-servers
                                           ::K/serializer.key :string
                                           ::K/serializer.value :string
                                           ::K.out/configuration {"client.id" "my-producer"}})]
-      (dotimes [_ load]
+      (doseq [n requestIds]
         (K.out/send producer
                     {::K/topic topic
                      ::K/headers {"requestType" (.getBytes requestType)
-                                  "requestId" (.getBytes (last (swap! requestId* conj (eval requestId))))
+                                  "requestId" (.getBytes n)
                                   "traceKey" (.getBytes (eval traceKey))}
                      ::K/value value}
                     (fn callback [exception _]
-                                       (when exception
-                                         "FAILURE"
-                                         ))))) @requestId*))
+                      (when exception
+                        "FAILURE")))))
+    requestIds))
 
-(defn consume-messages [conf ids]
-  (let [{:keys [kafka-servers response-topic]} conf
-        a-ids (atom ids)]
-    (with-open [consumer (K.in/consumer {::K/nodes              kafka-servers
-                                         ::K/deserializer.key   :string
-                                         ::K/deserializer.value :string
-                                         ::K.in/configuration   {"group.id"           "test-group"}})]
-      (K.in/register-for consumer
-                         [response-topic])
-      (while (not-empty @a-ids) (doseq [record (K.in/poll consumer
-                                                          {::K/timeout [5 :seconds]})]
-                                  (let [id (:requestId
-                                            (reduce
-                                             (fn [m [k v]] (assoc m (keyword k) (String. v)))
-                                             {}
-                                             (::K/headers record)))]
-                                    (swap! a-ids (fn [coll id] (remove #(= id %) coll)) id)
-                                    (println (format "Record %s" id))))))))
+(defn- consume-messages [{:keys [kafka-servers response-topic]} requestIds]
 
-(defn test-kafka
-  ([conf] (test-kafka conf (:message-number conf)))
-  ([conf load] (let [start-time (atom "")]
+  (with-open [consumer (K.in/consumer {::K/nodes              kafka-servers
+                                       ::K/deserializer.key   :string
+                                       ::K/deserializer.value :string
+                                       ::K.in/configuration   {"group.id"           "test-group"}})]
+    (K.in/register-for consumer
+                       [response-topic])
+    (loop [ids requestIds]
+      (println (count ids))
+      (when (not-empty ids)
+        (recur (-> (K.in/poll consumer
+                              {::K/timeout [5 :seconds]})
+                   (get-fn-requestId?)
+                   (remove ids)))))))
 
-                 (->> (send-messages conf load)
-                      (do (reset! start-time (Instant/now)))
-                      (consume-messages conf))
-
-                 (let [end-time (Instant/now)
-                       duration (Duration/between @start-time end-time)
-                       result-str (format "
+(defn- resume [start-time load]
+  (let [end-time (Instant/now)
+        duration (Duration/between start-time end-time)
+        result-str (format "
 ======================================
 load  %d
 start %s
 end   %s
 diff  %d sec
 diff  %d min"
-                                          load @start-time end-time
-                                          (.getSeconds duration)
-                                          (quot (.getSeconds duration) 60))]
+                           load start-time end-time
+                           (.getSeconds duration)
+                           (quot (.getSeconds duration) 60))]
 
-                   (println result-str)
-                   (spit "result.txt" result-str :append true)))))
+    (println result-str)
+    (spit "result.txt" result-str :append true)))
 
-(defn -main [& args]
-  (let [conf (get-config (first args))]
-    (if-let [load (second args)]
+(defn- eval-request-ids [conf load]
+  (repeatedly load #(eval (get-in conf [:headers :requestId]))))
+
+(defn- test-kafka [conf load]
+  (let [requestIds (eval-request-ids conf load)]
+    (send-messages conf requestIds)
+    (let [start-time (Instant/now)]
+      (println (str start-time " sended " load " messages"))
+      (consume-messages conf requestIds)
+      (resume start-time load))))
+
+(defn -main [properties & args]
+  (let [conf (get-config properties)]
+    (if-let [load (first args)]
       (dotimes [n (Integer/parseInt load)] (test-kafka conf (int (Math/pow 10 n))))
-      (test-kafka args)))
-  (println "The end..."))
+      (test-kafka conf (:message-number conf)))))
 
 (comment
-  (-main "/home/kromke/Документы/ЕГРН 2/tests-doc/sa-bft-dev.edn" "6")
-  (send-messages (get-config "/home/kromke/Документы/ЕГРН 2/tests-doc/sa-bft-dev.edn") 2))
+  (-main "/home/kromke/Документы/ЕГРН 2/tests-doc/sa-bft-dev.edn" "4")
+   )
